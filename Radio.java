e43de3d2e228c0aed0a257106fe3127ee44ee9a2
wilfred-23 <wilfredorellana9@gmail.com>
2026-01-23 09:35:29 -0600

    Update Radio.java

---
+++ Radio.java
@@ -0,0 +1,12 @@
+package radio;
+
+public interface Radio {
+
+    void prenderRadio();
+    void apagarRadio();
+    void cambiarAMFM();
+    void avanzarEmisora();
+    void guardarEmisora(int boton);
+    void cargarEmisora(int boton);
+
+}
