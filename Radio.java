9818cde0bf3b01a401fab8b62cf5758d98a1ae24
RaquelVega <52298462+RaquelVega@users.noreply.github.com>
2026-01-23 19:43:15 -0600

    Refactor Radio interface with improved documentation
    Updated interface to include detailed method documentation and changed method names for clarity.

--- Radio.java
+++ Radio.java
@@ -1,15 +1,50 @@
-package radio;
-
+/**
+ * Interfaz que define las operaciones básicas de un radio.
+ * Permite encender/apagar, cambiar entre AM/FM, avanzar estaciones,
+ * y guardar/cargar estaciones en botones programables.
+ * 
+ * @author Raquel Pamela Tejada Vega / Will Orellana
+ * @version 1.0
+ */
 public interface Radio {
 
+    /**
+     * Enciende el radio.
+     */
     void prenderRadio();
-    void apagarRadio();
-    void cambiarAMFM();
-    void avanzarEmisora();
-    void guardarEmisora(int boton);
-    void cargarEmisora(int boton);
 
-    boolean isEncendido();
-    boolean isFM();
-    double getEmisoraActual();
+    /**
+     * Apaga el radio.
+     */
+    void apagarRadio();
+
+    /**
+     * Avanza a la siguiente estación en el dial actual (FM o AM).
+     */
+    void avanzarEstacion();
+
+    /**
+     * Guarda la estación actual en un botón programable.
+     * 
+     * @param numeroBoton número del botón (1-12) donde guardar
+     */
+    void guardarEstacion(int numeroBoton);
+
+    /**
+     * Carga una estación previamente guardada en un botón.
+     * 
+     * @param numeroBoton número del botón (1-12) a cargar
+     */
+    void cargarEstacion(int numeroBoton);
+
+    /**
+     * Cambia el radio a modo FM.
+     */
+    void cambiarFM();
+
+    /**
+     * Cambia el radio a modo AM.
+     */
+    void cambiarAM();
 }
+
